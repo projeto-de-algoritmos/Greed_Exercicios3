da5b364d85acff25188fd5a30e768da1b9e47b3d
Matheus Henrique <matheushenriquep285@gmail.com>
2023-10-29 08:32:57 -0300

    fix: Corrige cálculo de retorno no problema 2141.

--- MaximumRunningTimeNComputers/2141.py
+++ MaximumRunningTimeNComputers/2141.py
@@ -1,3 +1,5 @@
+# Tipo de problema: Algoritmo de agendamento de intervalo.
+
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
 
@@ -7,11 +9,14 @@ class Solution:
         # Ordenando as baterias em ordem crescente
         batteries.sort()
 
-        media = (tempoBaterias // n)            # Pegando a media de tempo gasto pelos computadores
+        # Calculando a media de tempo gasto pelos computadores
+        media = (tempoBaterias // n)
+        
         # Verificando se a ultima bateria, a maior de todas, 
         # eh maior que o consumo medio
         while batteries[-1] > media:   
             tempoBaterias -= batteries.pop()    # Atualizando o tempo total restante para conter somente as baterias limitantes
             n -= 1                              # Atualizando o numero de baterrias
+            media = (tempoBaterias // n)        # Atualiza a media de tempo gasto pelos computadores
 
         return (tempoBaterias // n)
